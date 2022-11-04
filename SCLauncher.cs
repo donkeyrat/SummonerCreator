4049d41f24e2fa57544db644ae8b61294d77735b
donkeyrat <darklordbananas@gmail.com>
2022-11-04 16:08:15 -0700

    Update SCLauncher.cs

--- SCLauncher.cs
+++ SCLauncher.cs
@@ -2,7 +2,7 @@
 
 namespace SummonerCreator
 {
-	[BepInPlugin("teamgrad.summoner", "Summoner Creator", "1.0.3")]
+	[BepInPlugin("teamgrad.summoner", "Summoner Creator", "1.0.4")]
 	public class SCLauncher : BaseUnityPlugin
 	{
 		public SCLauncher()
