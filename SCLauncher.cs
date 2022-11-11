481c5c123389fefa8572f2aec3ee6829a2789058
donkeyrat <darklordbananas@gmail.com>
2022-11-11 10:44:18 -0800

    fixed bug with factions

--- SCLauncher.cs
+++ SCLauncher.cs
@@ -2,7 +2,7 @@
 
 namespace SummonerCreator
 {
-	[BepInPlugin("teamgrad.summoner", "Summoner Creator", "1.0.4")]
+	[BepInPlugin("teamgrad.summoner", "Summoner Creator", "1.0.5")]
 	public class SCLauncher : BaseUnityPlugin
 	{
 		public SCLauncher()
